4c96f08a9c9add2b199a2e1b446df2d822aa2bc8
LucyShawYang <lucyshawyang@outlook.com>
2019-12-17 23:33:38 +0800

    Manacher

--- cpp/5.最长回文子串.cpp
+++ cpp/5.最长回文子串.cpp
@@ -10,6 +10,31 @@ class Solution
 public:
     string longestPalindrome(string s)
     {
+        const int MAXN = 1001;
+        bool isPalin[MAXN][MAXN] = {0};
+        int length = s.size();
+        for (int i = 0; i < length; ++i)
+            isPalin[i][i] = true;
+        for (int i = 0; i < length - 1; ++i)
+        {
+            if (s[i] == s[i + 1])
+                isPalin[i][i + 1] = true;
+        }
+        for (int i = 2; i < length; ++i) // i是坐标差
+        {
+            for (int j = 0; j < length - i; ++j)
+                if (s[j] == s[j + i] && isPalin[j + 1][j + i - 1])
+                    isPalin[j][j + i] = true;
+        }
+        for (int i = length - 1; i >= 0; --i) // i是坐标差
+            for (int j = 0; j < length - i; ++j)
+                if (isPalin[j][j + i])
+                    return s.substr(j, i + 1);
+        return "";
+    }
+
+    string longestPalindrome1(string s)
+    {
         int length = s.size();
         for (int i = length; i >= 1; --i) // 枚举字串长度
         {
